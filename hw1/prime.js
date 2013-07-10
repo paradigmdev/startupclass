a0269cb1b57d058d622d4cf3d0a297ada2a69a75
Craig Kollegger <craig.kollegger@gmail.com>
2013-07-10 09:38:41 +0200

    Homeworks

--- hw1/prime.js
+++ hw1/prime.js
@@ -1,44 +1,35 @@
 #!/usr/bin/env node
+
 // Prime number generator
 // Accepts varible n to see if that number is prime
-var isPrime = function(n) {
-  // If it's less than 2 it's not prime
-  if(n < 2) { return false; }  
-
-  // get the int version of square root
-  var j = Math.floor(Math.sqrt(n));
-
-  // loop though and 
-  for(var i = 2; i <= j; i++) {
-    if(n % i == 0) {
-      return false;
-    } else {
-      return true;
-    }
-};
+function isPrime(n) {
+    if (n < 2) return false;
+    var m = Math.sqrt(n);
+    for (var i = 2;i <= m;i++) if (n % i == 0) return false;
+    return true;
+}
 
 // Find first K prime numbers via basic for loop
-var firstkprime = function(k) {
-  var i = 1;
-  var arr = [];
-  for(i = 1; i < k+1; i++) {
-    if isPrime(i) { arr.push(i); }
-  }
-  return arr;
+var firstKPrime = function(k) {
+    var arr = [];
+    var i = 1;
+    while (arr.length < k) {
+        if(isPrime(i)) {
+            arr.push(i);
+        }
+        i++;
+    }
+    return arr;
 };
 
 // Print to console
 var fmt = function(arr) {
-  return arr.join(",");
+    return arr.join(",");
 };
 
+// Write to the file
 var k = 100;
-console.log("firstkprime(" + k + ")");
-console.log(fmt(firstkprime(k)));
-
-
-//var fs = require('fs');
-//var outfile = "prime.txt";
-//var out = "A startup is a business built to grow rapidly.\n"
-//fs.writeFileSync(outfile, out);
-//console.log("Script: " + __filename + "\nWrote: " + out + "To: " + outfile);
+var fs = require('fs');
+var outfile = "prime.txt";
+var out = fmt(firstKPrime(k));
+fs.writeFileSync(outfile, out);
